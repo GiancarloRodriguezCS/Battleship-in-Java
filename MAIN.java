fa5962e5daf98b0170ae2b0882c6ac78f1dcc593
GiancarloRodriguezCS <105930315+GiancarloRodriguezCS@users.noreply.github.com>
2022-05-23 21:10:10 -0400

    Update MAIN.java

--- MAIN.java
+++ MAIN.java
@@ -1,5 +1,7 @@
+//This class runs the program
 public class MAIN {
 
+    //this method calls the Homescreen class in order to get everything running
     public static void main(String[] args) {
         homeScreen o = new homeScreen();
         o.setVisible(true);
