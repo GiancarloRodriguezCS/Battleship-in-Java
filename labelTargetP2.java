0c60112a630097ca67e4c672bcaf30f0e9b93e26
GiancarloRodriguezCS <105930315+GiancarloRodriguezCS@users.noreply.github.com>
2022-05-25 10:19:49 -0400

    Update labelTargetP2.java

--- labelTargetP2.java
+++ labelTargetP2.java
@@ -1,6 +1,7 @@
 import javax.swing.*;
 import java.util.ArrayList;
 
+//Handles putting ships, missed shots, and hit shots on the board to see
 public class labelTargetP2 {
 
     private JLabel label;
@@ -8,8 +9,10 @@ public class labelTargetP2 {
     private int yPos;
 
     public static ArrayList<labelTargetP2> registry;
+    //Getter for arraylist
     public static ArrayList<labelTargetP2> getRegistry() { return registry; }
 
+    //Constructor which gives a JLabel a position
     public labelTargetP2(JLabel label, int x, int y) {
         if(registry == null) registry = new ArrayList<>();
         this.label = label;
@@ -26,6 +29,7 @@ public class labelTargetP2 {
         registry.add(this);
     }
 
+    //Getters
     public JLabel getLabel() {
         return label;
     }
